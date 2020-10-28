f18dff4e0bfabebc5b51bdf592858239b00faec7
ajakcyer <ajakcyer97@gmail.com>
2020-10-28 18:36:57 -0400

    done

--- index.js
+++ index.js
@@ -40,7 +40,48 @@ function toggleColor(element) {
   }
 }
 
+//find dom object
+const header = document.querySelector('#header')
+//add listener to object
+header.addEventListener('click', function(){
+  toggleColor(header)
+  // click
+  // work
+})
 
 /***** Deliverable 2 *****/
+//find form object
+const form = document.querySelector('#new-player-form')
+const submitBtn = form.querySelector('[type=submit]')
+
+form.addEventListener('submit', function(e){
+  let player = {}
+  const formInfo = e.target
+  player["number"] = formInfo.number.value
+  player["name"] = formInfo.name.value 
+  player["nickname"] = formInfo.nickname.value
+  player["photo"] = formInfo.photo.value
+  player["likes"] = 0
+  renderPlayer(player)
+  e.preventDefault()
+  formInfo.reset()
+})
+//prevent default event on the form so it don't make a request
+  //submit event
+//use renderPlayer function to add a player to bottom of page
+  // need players in the same format
+
 
 /***** Deliverable 3 *****/
+const playerParentCont = document.querySelector('.player-container')
+
+playerParentCont.addEventListener('click', function(e){
+
+  if (e.target.matches('.like-button')) {
+    const playerParent = e.target.parentNode
+    let likes = playerParent.querySelector('.likes')
+    let addLikes = parseInt(likes.textContent)
+    likes.textContent = `${addLikes + 1} likes`
+
+  }
+})
